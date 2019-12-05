c8d67ea008499940c81b644d2df01d0a42f47a02
danyueming <923306868@qq.com>
2019-12-05 19:31:51 +0800

    first

---
+++ WpfApp1/MainwindowVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MainwindowVM : BaseModel
+    {
+
+        /// <summary>
+        /// 第一层的集合
+        /// </summary>
+        public ObservableCollection<TreeViewItemModel> MenuItems { get; } = new ObservableCollection<TreeViewItemModel>();
+        public MainwindowVM()
+        {
+            MenuItems = new ObservableCollection<TreeViewItemModel>()
+            {
+                new TreeViewItemModel("label1","doctor"),
+  new TreeViewItemModel("label2","doctor"),
+  new TreeViewItemModel("label3","polic"){CustomItems=new ObservableCollection<TreeViewItemModel>
+  {new TreeViewItemModel("button","test1"),
+  new TreeViewItemModel("textbox","test2"),
+  new TreeViewItemModel("combox","test3")} ,
+ }
+            };
+        }
+    }
+}
---
+++ WpfApp1/TreeViewItemModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class TreeViewItemModel : BaseModel
+    {
+
+        public TreeViewItemModel(string header, string tag)
+        {
+            Header = header;
+            Tag = tag;
+            CustomItems = new ObservableCollection<TreeViewItemModel>();
+        }
+
+        public string Header { get; set; }
+
+        public string Tag { get; set; }
+
+        public Visibility Visibility
+        {
+            get => _visibility;
+            set { _visibility = value; NotifyPropertyChanged(); }
+        }
+        private Visibility _visibility = Visibility.Visible;
+
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set { _isExpanded = value; NotifyPropertyChanged(); }
+        }
+        private bool _isExpanded = true;
+
+        /// <summary>
+        /// 第二层的集合
+        /// </summary>
+        public ObservableCollection<TreeViewItemModel> CustomItems
+        {
+            get => _customItems;
+            set { _customItems = value; NotifyPropertyChanged(); }
+        }
+        private ObservableCollection<TreeViewItemModel> _customItems;
+    }
+
+
+}
